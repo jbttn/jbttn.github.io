3554ad6ac21ffa006034849a019d12efc1c8d934
Joshua Button <deathnoodle@gmail.com>
2012-08-15 15:34:56 -0700

    A lot of about page updates

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 $('#triangles').plaxify({"xRange":180,"yRange":40})
 //$.plax.enable()
+
+/*
+$.getJSON('/skills.json', function(data) {
+  var items = [];
+
+  console.log("data: " + data.languages[0].url);
+  
+  $.each(data.languages, function(key, val) {
+    items.push('<li><a href="' + val.url + '" class="' + val.skill_level + '">' + val.name + '</a></li>');
+  });
+
+  $('<ul/>', {
+    'id': 'languages',
+    html: items.join('')
+  }).appendTo('#skills');
+});*/
